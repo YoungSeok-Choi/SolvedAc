bd312bb6dfca51a69f7fdb9d19b0b71d74a92d1e
최용석 <58320695+YoungSeok-Choi@users.noreply.github.com>
2021-08-09 03:00:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+  chessBoard = []
+
+  chessBoard = input()
+
+
+
+
+def sliceSqure(chessBoard):
+
+            
+if __name__ == "__main__":
+    main()
+
+
+  # iterNumber = int(input())
+  # unSortList = []
+  # for i in range(0, iterNumber):
+  #   tempInput = int(input())
+  #   unSortList.append(tempInput)
+  
+  # afterSort = sorted(unSortList, reverse=True)
+
+  # for i in afterSort:
+  #   print(i)
+
